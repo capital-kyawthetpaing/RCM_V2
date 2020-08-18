98f732dbe2181a74bf27c355eb326c7af039d97e
kyawthetpaing <kyawthetpaing@mm.gmail.com>
2020-08-18 16:26:56 +0630

    ktp

--- DL/ReadIni.cs
+++ DL/ReadIni.cs
@@ -20,10 +20,10 @@
         {
             IniFileReader ifd = new IniFileReader(filePath);
 
-            return "Data Source=" + ifd.IniReadValue("Database", "Accele").Split(',')[0] +
-                    ";Initial Catalog=" + ifd.IniReadValue("Database", "Accele").Split(',')[1] +
-                    ";Persist Security Info=True;User ID=" + ifd.IniReadValue("Database", "Accele").Split(',')[2] +
-                    ";Password=" + ifd.IniReadValue("Database", "Accele").Split(',')[3] +
+            return "Data Source=" + ifd.IniReadValue("Database", "RCM_V2").Split(',')[0] +
+                    ";Initial Catalog=" + ifd.IniReadValue("Database", "RCM_V2").Split(',')[1] +
+                    ";Persist Security Info=True;User ID=" + ifd.IniReadValue("Database", "RCM_V2").Split(',')[2] +
+                    ";Password=" + ifd.IniReadValue("Database", "RCM_V2").Split(',')[3] +
                     ";";
         }
     }
