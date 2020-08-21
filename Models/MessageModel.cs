33a4b28c315791bafa6374b83c5a59f809619171
kyawthetpaing <kyawthetpaing@mm.gmail.com>
2020-08-21 14:26:23 +0630

    ktp

---
+++ Models/MessageModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Models
+{
+    public class MessageModel : BaseModel
+    {
+        public string MessageID { get; set; }
+        public string MessageIcon { get; set; }
+        public string MessageText { get; set; }
+    }
+}
---
+++ RCM_V2/Controllers/MessageApiController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Models;
+using Message_BL;
+
+namespace RCM_V2.Controllers
+{
+    public class MessageApiController : ApiController
+    {
+        [UserAuthentication]
+        [HttpPost]
+        public IHttpActionResult Message_Select([FromBody] MessageModel messageModel)
+        {
+            MessageBL messageBL = new MessageBL();
+            return Ok(messageBL.Message_Select(messageModel));
+        }
+    }
+}
