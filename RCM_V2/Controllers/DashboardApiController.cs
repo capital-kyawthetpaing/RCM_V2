aafc24a8b5f906834c91e384ab8c80e5373bc3e7
Ohnmar Kyi <capital.ohnmarkyi@gmail.com>
2020-08-28 17:36:31 +0630

    omk

--- RCM_V2/Controllers/DashboardApiController.cs
+++ RCM_V2/Controllers/DashboardApiController.cs
@@ -4,10 +4,19 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Notice_BL;
 
 namespace RCM_V2.Controllers
 {
     public class DashboardApiController : ApiController
     {
+        [UserAuthentication]
+        [HttpPost]
+        [ActionName("Notice_Select")]
+        public IHttpActionResult Notice_Select()
+        {
+            NoticeBL bl = new NoticeBL();
+            return Ok(bl.NoticeSelect());
+        }
     }
 }
