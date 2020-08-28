f275707f2b3fa78e45641e69385826c54e63822a
kyawthetpaing <kyawthetpaing@mm.gmail.com>
2020-08-28 11:09:01 +0630

    ktp

---
+++ Models/ExportFieldsModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Models
+{
+    public class ExportFieldsModel : BaseModel
+    {
+        public string ExportName { get; set; }
+        public string ExportValue { get; set; }
+        public string ExportType { get; set; }
+    }
+}
---
+++ RCM_V2/Controllers/ExportFieldController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RCM_V2.Controllers
+{
+    public class ExportFieldController : Controller
+    {
+        // GET: ExportField
+        public ActionResult ExportFieldEntry()
+        {
+            return View();
+        }
+
+        public ActionResult ExportFieldList()
+        {
+            return View();
+        }
+    }
+}
