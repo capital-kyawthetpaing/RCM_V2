4037c21bf5155a8b090da7d098b972cc16090750
Ohnmar Kyi <capital.ohnmarkyi@gmail.com>
2020-08-31 13:42:58 +0630

    omk(31-08-2020)

---
+++ RCM_V2/Controllers/CategoryApiController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Models;
+using Category_BL;
+
+namespace RCM_V2.Controllers
+{
+    public class CategoryApiController : ApiController
+    {
+        [UserAuthentication]
+        [HttpPost]
+        [ActionName("MallCategory_Select")]
+        public IHttpActionResult MallCategory_Select(MallCategoryModel mallCategoryModel)
+        {
+            CategoryBL bl = new CategoryBL();
+            return Ok(bl.MallCategory_Select(mallCategoryModel));
+        }
+    }
+}
