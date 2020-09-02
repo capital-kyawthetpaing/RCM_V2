bd7aa12fce37aba9d3952e0bdec23bca164bceea
Ohnmar Kyi <capital.ohnmarkyi@gmail.com>
2020-09-02 17:54:47 +0630

    omk

--- Models/ExportFieldsModel.cs
+++ Models/ExportFieldsModel.cs
@@ -1,9 +1,15 @@
-﻿namespace Models
+﻿using System.Collections.Generic;
+
+namespace Models
 {
     public class ExportFieldsModel : BaseModel
     {
         public string ExportName { get; set; }
         public string ExportValue { get; set; }
         public string ExportType { get; set; }
+        public List<string> TableColumnList { get; set; }
+        public List<string> FormColumnList { get; set; }
+        public string TableColumnJson { get; set; }
+        public string FormColumnJson { get; set; }
     }
 }
---
+++ RCM_V2/Controllers/ExportFieldApiController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Models;
+using ExportField_BL;
+
+namespace RCM_V2.Controllers
+{
+    public class ExportFieldApiController : ApiController
+    {
+        [UserAuthentication]
+        [HttpPost]
+        [ActionName("ExportField_Save")]
+        public IHttpActionResult ExportField_Save(ExportFieldsModel exportFieldsModel)
+        {
+            ExportFieldBL bl = new ExportFieldBL();
+            return Ok();
+        }
+    }
+}
