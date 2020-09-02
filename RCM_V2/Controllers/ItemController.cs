512e182c42397b255f3372592909dd78d209185f
kyawthetpaing <kyawthetpaing@mm.gmail.com>
2020-09-02 09:04:27 +0630

    ktp

---
+++ RCM_V2/Controllers/ItemController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RCM_V2.Controllers
+{
+    public class ItemController : Controller
+    {
+        // GET: Item
+        public ActionResult ItemManageList()
+        {
+            return View();
+        }
+    }
+}
