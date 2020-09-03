6319d6efd6b42089219f5b29cbbee77c7750282e
Ohnmar Kyi <capital.ohnmarkyi@gmail.com>
2020-09-03 17:48:33 +0630

    omk

--- ExportField_BL/ExportFieldBL.cs
+++ ExportField_BL/ExportFieldBL.cs
@@ -22,17 +22,17 @@ namespace ExportField_BL
 
         public string ExportField_Save(ExportFieldsModel exportFieldsModel)
         {
-            BaseDL bdl = new BaseDL();           
-           // exportFieldsModel.ExportFieldJson = JsonConvert.SerializeObject(exportFieldsModel.ExportFieldList);
-            exportFieldsModel.Sqlprms= new SqlParameter[8];
+            BaseDL bdl = new BaseDL(); 
+            exportFieldsModel.Sqlprms= new SqlParameter[9];
             exportFieldsModel.Sqlprms[0] = new SqlParameter("@ExportName", SqlDbType.VarChar) { Value = exportFieldsModel.ExportName };
-            exportFieldsModel.Sqlprms[1] = new SqlParameter("@Status", SqlDbType.VarChar) { Value = exportFieldsModel.Status };
-            //exportFieldsModel.Sqlprms[2] = new SqlParameter("@ExportFieldJson", SqlDbType.VarChar) { Value = exportFieldsModel.ExportFieldJson };
-            exportFieldsModel.Sqlprms[3] = new SqlParameter("@CreatedBy", SqlDbType.VarChar) { Value = exportFieldsModel.CreatedBy };
-            exportFieldsModel.Sqlprms[4] = new SqlParameter("@UpdatedBy", SqlDbType.VarChar) { Value = exportFieldsModel.UpdatedBy };
-            exportFieldsModel.Sqlprms[5] = new SqlParameter("@ExportType", SqlDbType.VarChar) { Value=exportFieldsModel.ExportType};
-            exportFieldsModel.Sqlprms[6] = new SqlParameter("@Mode", SqlDbType.VarChar) {Value=exportFieldsModel.Mode};
-            exportFieldsModel.Sqlprms[7] = new SqlParameter("@AutoID", SqlDbType.VarChar) { Value = exportFieldsModel.AutoID };
+            exportFieldsModel.Sqlprms[1] = new SqlParameter("@ExportValue", SqlDbType.VarChar) { Value = exportFieldsModel.ExportValue };
+            exportFieldsModel.Sqlprms[2] = new SqlParameter("@ExportValueID", SqlDbType.VarChar) { Value = exportFieldsModel.ExportValueID };
+            exportFieldsModel.Sqlprms[3] = new SqlParameter("@Status", SqlDbType.VarChar) { Value = exportFieldsModel.Status };
+            exportFieldsModel.Sqlprms[4] = new SqlParameter("@CreatedBy", SqlDbType.VarChar) { Value = exportFieldsModel.CreatedBy };
+            exportFieldsModel.Sqlprms[5] = new SqlParameter("@UpdatedBy", SqlDbType.VarChar) { Value = exportFieldsModel.UpdatedBy };
+            exportFieldsModel.Sqlprms[6] = new SqlParameter("@ExportType", SqlDbType.VarChar) { Value=exportFieldsModel.ExportType};
+            exportFieldsModel.Sqlprms[7] = new SqlParameter("@Mode", SqlDbType.VarChar) {Value=exportFieldsModel.Mode};
+            exportFieldsModel.Sqlprms[8] = new SqlParameter("@AutoID", SqlDbType.VarChar) { Value = exportFieldsModel.AutoID };
             return bdl.InsertUpdateDeleteData("ExportColumn_CUD", exportFieldsModel.Sqlprms);
         }
 
@@ -52,6 +52,8 @@ namespace ExportField_BL
                 exportFieldsModel.ExportName = dr["ExportName"].ToString();
                 exportFieldsModel.Status = dr["Status"].ToString();
                 exportFieldsModel.ExportType = dr["ExportType"].ToString();
+                exportFieldsModel.ExportValue = dr["ExportValue"].ToString();
+                exportFieldsModel.ExportValueID = dr["ExportValueID"].ToString();
             }
             return exportFieldsModel;
         }
