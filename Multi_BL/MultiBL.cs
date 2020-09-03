8479aa696188fb982e65ada7f6e4d5a0cf8c036e
Ay Mi Soe <Ay Mi Soe@DESKTOP-DJ5ADD9>
2020-09-03 14:48:00 +0630

    byami

--- Multi_BL/MultiBL.cs
+++ Multi_BL/MultiBL.cs
@@ -1,9 +1,20 @@
-﻿using System;
+﻿using DL;
+using System;
 using System.Collections.Generic;
 namespace Multi_BL
 {
     public class MultiBL
     {
+        public string Year_Select()
+        {
+            BaseDL bdl = new BaseDL();
+            return bdl.SelectJson("Year_Select", null);
+        }
 
+        public string Season_Select()
+        {
+            BaseDL bdl = new BaseDL();
+            return bdl.SelectJson("Season_Select",null);
+        }
     }
 }
