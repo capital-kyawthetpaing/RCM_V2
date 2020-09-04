92969dfad365f96f630dc13234a36cc14b96d3d5
capital-hnineithu <capital.hnineithu@gmail.com>
2020-09-04 17:38:56 +0630

    byeithu

--- Shop_ItemNamePriority_BL/Shop_itemPriorityBL.cs
+++ Shop_ItemNamePriority_BL/Shop_itemPriorityBL.cs
@@ -2,6 +2,8 @@
 using Models;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,8 +15,9 @@ namespace Shop_ItemNamePriority_BL
         public string ShopPriority_Select(Shop_ItemNamePriorityModel shopModel)
         {
             BaseDL bdl = new BaseDL();
-
-            return bdl.SelectJson("ShopPriority_Select", null);
+            shopModel.Sqlprms = new SqlParameter[1];
+            shopModel.Sqlprms[0] = new SqlParameter("@ShopID", SqlDbType.VarChar) { Value = shopModel.ShopID };
+            return bdl.SelectJson("ShopPriority_Select", shopModel.Sqlprms);
         }
     }
 }
