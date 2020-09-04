d3df529a32daba2e4b6ae7d6f30de91602a4b7d1
kyawthetpaing <kyawthetpaing@mm.gmail.com>
2020-09-04 15:10:54 +0630

    ktp

---
+++ Models/Shop_ItemNamePriorityModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Models
+{
+    public class Shop_ItemNamePriorityModel : BaseModel
+    {
+        public string ShopID { get; set; }
+        public string PriorityValue { get; set; }
+        public string PriorityValueID { get; set; }
+    }
+}
