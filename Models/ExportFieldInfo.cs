559ffe664646d27a0dbbf986a3299fce9f59a0b2
Ohnmar Kyi <capital.ohnmarkyi@gmail.com>
2020-09-07 09:19:57 +0630

    omk

---
+++ Models/ExportFieldInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class ExportFieldInfo
+    {
+        public string ID { get; set; }
+        public string ExportValueID { get; set; }
+        public string ControlID { get; set; }
+    }
+}
