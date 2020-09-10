3e710e5e88c61b37976feeba96b9585938d03374
Ay Mi Soe <Ay Mi Soe@DESKTOP-DJ5ADD9>
2020-09-10 17:33:10 +0630

    byami

--- Item_BL/ItemBL.cs
+++ Item_BL/ItemBL.cs
@@ -11,14 +11,16 @@ namespace Item_BL
         public string ItemManage_List(ItemModel itemModel)
         {
             BaseDL bdl = new BaseDL();
-            itemModel.Sqlprms = new SqlParameter[7];
-            itemModel.Sqlprms[0] = new SqlParameter("@ItemName", SqlDbType.VarChar) { Value = itemModel.ItemName };
-            itemModel.Sqlprms[1] = new SqlParameter("@MakerCD", SqlDbType.VarChar) { Value = itemModel.MakerCD };
-            itemModel.Sqlprms[2] = new SqlParameter("@YearCD", SqlDbType.VarChar) { Value = itemModel.YearCD };
-            itemModel.Sqlprms[3] = new SqlParameter("@SeasonCD", SqlDbType.VarChar) { Value = itemModel.SeasonCD };
-            itemModel.Sqlprms[4] = new SqlParameter("@CatalogInfo", SqlDbType.VarChar) { Value = itemModel.CatalogInfo };
-            itemModel.Sqlprms[5] = new SqlParameter("@ClassName", SqlDbType.VarChar) { Value = itemModel.ClassName };
-            itemModel.Sqlprms[6] = new SqlParameter("@CompetitionName", SqlDbType.VarChar) { Value = itemModel.CompetitionName };
+            itemModel.Sqlprms = new SqlParameter[9];
+            itemModel.Sqlprms[0] = new SqlParameter("@BrandName", SqlDbType.VarChar) { Value = itemModel.BrandName };
+            itemModel.Sqlprms[1] = new SqlParameter("@ItemName", SqlDbType.VarChar) { Value = itemModel.ItemName };
+            itemModel.Sqlprms[2] = new SqlParameter("@MakerCD", SqlDbType.VarChar) { Value = itemModel.MakerCD };
+            itemModel.Sqlprms[3] = new SqlParameter("@MakerName", SqlDbType.VarChar) { Value = itemModel.MakerName };
+            itemModel.Sqlprms[4] = new SqlParameter("@YearCD", SqlDbType.VarChar) { Value = itemModel.YearCD };
+            itemModel.Sqlprms[5] = new SqlParameter("@SeasonCD", SqlDbType.VarChar) { Value = itemModel.SeasonCD };
+            itemModel.Sqlprms[6] = new SqlParameter("@CatalogInfo", SqlDbType.VarChar) { Value = itemModel.CatalogInfo };
+            itemModel.Sqlprms[7] = new SqlParameter("@ClassName", SqlDbType.VarChar) { Value = itemModel.ClassName };
+            itemModel.Sqlprms[8] = new SqlParameter("@CompetitionName", SqlDbType.VarChar) { Value = itemModel.CompetitionName };
             return bdl.SelectJson("ItemManage_Select", itemModel.Sqlprms);
         }
 
